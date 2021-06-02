9ffbdf8f7152740ab1a34887afc6abf3895ab8dd
Luis Montoya <luis.montoya@exatec.tec.mx>
2021-06-02 13:14:30 -0500

    [FEAT] CHPT for debugging colors

--- opencv/color_detection.py
+++ opencv/color_detection.py
@@ -1,26 +1,72 @@
 import numpy as np
 import cv2 as cv
-  
-  
-# Capturing video through webcam
+
+########            COLORS            ########
+# Limit for robot color
+robot_color     = [(80, 50, 80), (140, 255, 255)]
+# Limits for team colors
+t1_color        = [(130, 50, 50), (160, 255, 255)]     # Purple ish
+t2_color        = [(80, 50, 60), (100, 255, 255)]      # Yellow-ish
+teams_colors    = [t1_color, t2_color]
+# Limits for role colors
+goal_color      = [(0, 0, 0), (255, 255, 255)]       # Red-ish
+mid_color       = goal_color     # Green-ish
+for_color       = goal_color    # Blue-ish
+
+# goal_color      = [(156, 0, 0), (200, 255, 255)]       # Red-ish
+# mid_color       = [(50, 150, 200), (70, 255, 255)]     # Green-ish
+# for_color       = [(110, 100, 60), (125, 255, 255)]    # Blue-ish
+
+role_colors     = [goal_color, mid_color, for_color]
+
+########            DETECTION INFORMATION            ########
+t1_info = {
+    "G": [(0, 0), 0],
+    "F": [(0, 0), 0],
+    "M": [(0, 0), 0],
+    "B": (0, 0)
+}
+t2_info = {
+    "G": [(0, 0), 0],
+    "F": [(0, 0), 0],
+    "M": [(0, 0), 0],
+    "B": (0, 0)
+}
+
+########            STREAM PARAMETERS            ########
 obs_camera = cv.VideoCapture(2)
 obs_camera.set(cv.CAP_PROP_FRAME_WIDTH, 1280)
 obs_camera.set(cv.CAP_PROP_FRAME_HEIGHT, 720)
 
-# Set range for teams
-robot_low  = (80, 50, 80)
-robot_high = (140, 255, 255)
-# Limits for teams
-t1_color = [(130, 50, 50), (160, 255, 255)]     # Purple ish
-t2_color = [(80, 50, 60), (100, 255, 255)]      # Yellow-ish
-teams_colors = [t1_color, t2_color]
 
-def robot_present(img, robot, threshold=0.8):
+
+def robot_present(img, threshold=0.8):
     total = np.float(np.sum(img))
     if total/img.size  > (1-threshold):
         return True
     else:
         return False
+
+def get_role(img_hsv, robot_cropped, robot):
+    # counter = 1
+    # plt.figure(figsize=(20, 20))
+    for j in range(len(role_colors)):
+        mask = cv.inRange(img_hsv, role_colors[j][0], role_colors[j][1]) 
+        result = cv.bitwise_and(robot_cropped, robot_cropped, mask=mask)
+
+        # plt.subplot(1, 18, counter) # this can be deleted
+        # plt.imshow(result)
+        # counter = counter + 1
+
+        # Classify by teams
+        if robot_present(result):
+            cv.imshow(str(robot), result)
+            if j == 0:
+                return 'G'
+            if j == 1:
+                return 'M'
+            if j == 2:
+                return "F"
 
 
 if not obs_camera.isOpened():
@@ -41,7 +87,7 @@ while True:
     # Convert To HSV
     hsvFrame = cv.cvtColor(imageFrame, cv.COLOR_BGR2HSV)
     # Create Robot Mask
-    robot_mask = cv.inRange(hsvFrame, robot_low, robot_high) 
+    robot_mask = cv.inRange(hsvFrame, robot_color[0], robot_color[1]) 
     # Fill holes
     contours, hierarchy = cv.findContours(robot_mask, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
     for pic, contour in enumerate(contours):
@@ -55,19 +101,18 @@ while True:
     robots_boundRect        = [None]*len(robots_contours)
     robots_centers          = [None]*len(robots_contours)
     robots_radius           = [None]*len(robots_contours)
+    robots_cropped          = [None]*len(robots_contours)   # Save images of robot region
     for i, c in enumerate(robots_contours):
         robots_contours_poly[i] = cv.approxPolyDP(c, 3, True)
         robots_boundRect[i] = cv.boundingRect(robots_contours_poly[i])
         robots_centers[i], robots_radius[i] = cv.minEnclosingCircle(robots_contours_poly[i])
-
-    robots_cropped = [None]*len(robots_contours)
-    
-    # RoI of individual robots
-    for i, c in enumerate(robots_contours):
         robots_cropped[i] = robots_img[robots_boundRect[i][1]:robots_boundRect[i][1] + robots_boundRect[i][3], robots_boundRect[i][0]:robots_boundRect[i][0] + robots_boundRect[i][2]].copy()
     
     t1_robots = []
     t2_robots = []
+
+    t1_roles = []
+    t2_roles = []
 
     for i, c in enumerate(robots_cropped):
         img_hsv = cv.cvtColor(robots_cropped[i], cv.COLOR_RGB2HSV)
@@ -76,17 +121,25 @@ while True:
             mask = cv.inRange(img_hsv, teams_colors[j][0], teams_colors[j][1]) 
             result = cv.bitwise_and(robots_cropped[i], robots_cropped[i], mask=mask)
             # Classify by teams
-            if robot_present(result, i):
+            if robot_present(result):
                 if j == 0:
                     t1_robots.append(i)
-                    cv.putText(robots_img, "T1", (int(robots_centers[i][0]), int(robots_centers[i][1])),
+                    t1_roles.append(get_role(img_hsv, robots_cropped[i], i))
+                    # Draw identifiers on screen
+                    cv.putText(robots_img, "T1:", (int(robots_centers[i][0]), int(robots_centers[i][1])),
+                        cv.FONT_HERSHEY_SIMPLEX, 0.5, (248, 65, 209), 2)
+                    cv.putText(robots_img, t1_roles[-1], (int(robots_centers[i][0] + 30), int(robots_centers[i][1])),
                         cv.FONT_HERSHEY_SIMPLEX, 0.5, (248, 65, 209), 2)
                 else:
                     t2_robots.append(i)
+                    t2_roles.append(get_role(img_hsv, robots_cropped[i], i))
+                    # Draw identifiers on screen
                     cv.putText(robots_img, "T2", (int(robots_centers[i][0]), int(robots_centers[i][1])),
                         cv.FONT_HERSHEY_SIMPLEX, 0.5, (57, 242, 248), 2) # BGR
+                    cv.putText(robots_img, t2_roles[-1], (int(robots_centers[i][0] + 30), int(robots_centers[i][1])),
+                        cv.FONT_HERSHEY_SIMPLEX, 0.5, (57, 242, 248), 2) # BGR
 
-    print(" {} {}".format(t1_robots, t2_robots))
+    print("t1 {} {} t2 {} {}".format(t1_robots, t1_roles, t2_robots, t2_roles))
     
     
 
